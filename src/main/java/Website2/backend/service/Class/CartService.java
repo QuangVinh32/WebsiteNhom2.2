013604b7feb1ea3fc8213000baf14ad568f9e7c4
MTizzzz <110175866+MTizzzz@users.noreply.github.com>
2024-06-16 01:53:30 +0700

    Update Type,Nsx

--- src/main/java/Website2/backend/service/Class/CartService.java
+++ src/main/java/Website2/backend/service/Class/CartService.java
@@ -1,7 +1,6 @@
 package Website2.backend.service.Class;
 
-import Website2.backend.Form.CartCreateForm;
-import Website2.backend.Form.CartUpdateForm;
+
 import Website2.backend.model.entity.Cart;
 import Website2.backend.repository.CartRepository;
 import Website2.backend.service.ICartService;
@@ -14,40 +13,5 @@ import java.util.List;
 
 @Service
 public class CartService implements ICartService {
-    @Autowired
-    private CartRepository cartRepository;
 
-    @Autowired
-    private ModelMapper modelMapper;
-
-    @Override
-    public List<Cart> getAllCartByUserId(Integer userId) {
-        return cartRepository.fingByUserId(userId);
-    }
-
-    @Override
-    @Transactional
-    public Cart createCart(CartCreateForm form) {
-        Cart cart = modelMapper.map(form , Cart.class);
-        Cart.CartPK cartId = new Cart.CartPK();
-        cartId.setProductId(form.getProductId());
-        cartId.setUserId(form.getUserId());
-        cart.setCartId(cartId);
-        return cartRepository.save(cart);
-    }
-
-    @Override
-    @Transactional
-    public Cart updateCart(CartUpdateForm form) {
-        Cart.CartPK id = new Cart.CartPK();
-        id.setProductId(form.getProductId());
-        id.setUserId(form.getUserId());
-        Cart cart = cartRepository.findById(id).get();
-        return null;
-    }
-
-    @Override
-    public void deleteCart(Integer userId, Integer productId) {
-
-    }
 }
--- src/main/java/Website2/backend/service/ICartService.java
+++ src/main/java/Website2/backend/service/ICartService.java
@@ -1,7 +1,6 @@
 package Website2.backend.service;
 
-import Website2.backend.Form.CartCreateForm;
-import Website2.backend.Form.CartUpdateForm;
+
 import Website2.backend.model.entity.Cart;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +9,5 @@ import java.util.List;
 
 public interface ICartService {
 
-    List<Cart> getAllCartByUserId(Integer userId);
 
-    Cart createCart(CartCreateForm form);
-
-    Cart updateCart(CartUpdateForm form);
-
-    void deleteCart(Integer userId, Integer productId);
 }
