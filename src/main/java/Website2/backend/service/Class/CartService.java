de0ea9380bece9741086da63618a44f72b4ab6aa
Le Quang Vinh <152072416+quangvinh37@users.noreply.github.com>
2024-06-25 14:56:06 +0700

    add 25/6

--- src/main/java/Website2/backend/service/Class/CartService.java
+++ src/main/java/Website2/backend/service/Class/CartService.java
@@ -1,5 +1,6 @@
 package Website2.backend.service.Class;
 import Website2.backend.model.entity.Cart;
+import Website2.backend.model.entity.Product;
 import Website2.backend.model.request.CreateCart;
 import Website2.backend.model.request.UpdateCart;
 import Website2.backend.repository.CartRepository;
@@ -32,12 +33,24 @@ public class CartService implements ICartService {
 
     @Override
     public void createCart(CreateCart createCart) throws Exception {
-
+        Optional<Cart> existingCart = cartRepository.findById(createCart.getCartId());
+        if (existingCart.isEmpty()){
+            throw new Exception(".....");
+        }
+        Cart cartDb = mapper.map(createCart, Cart.class);
+        cartRepository.save(cartDb);
     }
 
     @Override
     public Cart updateCart(int id, UpdateCart updateCart) throws Exception {
-        return null;
+        Optional<Cart> cartDb = findByCartId(id);
+        if (cartDb.isPresent()){
+            Cart existingCart = cartDb.get();
+            mapper.map(updateCart, existingCart);
+            return cartRepository.save(existingCart);
+        }else {
+            throw new Exception("Product không tìm thấy id");
+        }
     }
 
     @Override
